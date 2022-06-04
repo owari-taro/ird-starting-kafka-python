a232c2ad8e02f833edc2b379d5ab6845059bcd2e
Kentaro Sasaki <accelerk@gmail.com>
2022-06-04 18:57:43 +0900

    feat: Create connect cluster and source connector

---
+++ setup/docker/connect-cluster/data/postgresql/create-tables.sql
@@ -0,0 +1,23 @@
+CREATE TABLE users (
+  user_id serial NOT NULL,
+  registered_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY (user_id)
+);
+
+CREATE TABLE contents (
+  content_id serial NOT NULL,
+  title varchar(100) NOT NULL,
+  published_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY (content_id)
+);
+
+CREATE TABLE ticket_orders (
+  order_id serial NOT NULL,
+  user_id integer NOT NULL,
+  content_id integer NOT NULL,
+  created_timestamp TIMESTAMP DEFAULT CURRENT_TIMESTAMP NOT NULL,
+  PRIMARY KEY (order_id),
+  FOREIGN KEY (user_id) REFERENCES users (user_id),
+  FOREIGN KEY (content_id) REFERENCES contents (content_id)
+);
+
