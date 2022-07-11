5dbc5d9e81cac9a349f3039a2f58b93f19adf31f
Kentaro Sasaki <accelerk@gmail.com>
2022-07-11 22:48:57 +0900

    feat: Use config key with StreamsConfig

--- stream-aggregator-app/src/main/java/com/example/StreamApp.java
+++ stream-aggregator-app/src/main/java/com/example/StreamApp.java
@@ -7,6 +7,7 @@ import org.apache.kafka.common.serialization.Serdes;
 import org.apache.kafka.streams.KafkaStreams;
 import org.apache.kafka.streams.KeyValue;
 import org.apache.kafka.streams.StreamsBuilder;
+import org.apache.kafka.streams.StreamsConfig;
 import org.apache.kafka.streams.Topology;
 import org.apache.kafka.streams.kstream.Consumed;
 import org.apache.kafka.streams.kstream.KStream;
@@ -27,72 +28,76 @@ import com.example.serdes.TicketOrderSerdes;
  * ストリーム処理のエントリーポイント
  */
 public class StreamApp {
-    private static final Logger logger = LoggerFactory.getLogger(StreamApp.class.getName());
-    private static final String BOOTSTRAP_SERVERS = "127.0.0.1:29092";
-    private static final String SOURCE_TOPIC = "category-filtered-ticket-order";
-    private static final String SINK_TOPIC = "suspicious-user";
+        private static final Logger logger = LoggerFactory.getLogger(StreamApp.class.getName());
+        private static final String BOOTSTRAP_SERVERS = "127.0.0.1:29092";
+        private static final String SOURCE_TOPIC = "category-filtered-ticket-order";
+        private static final String SINK_TOPIC = "suspicious-user";
 
-    public static void main(String[] args) {
-        // プロセッサー・トポロジーをビルド
-        Topology topology = buildTopology();
-        logger.info(topology.describe().toString());
+        public static void main(String[] args) {
+                // プロセッサー・トポロジーをビルド
+                Topology topology = buildTopology();
+                logger.info(topology.describe().toString());
 
-        // 設定値を取得
-        Properties streamConfig = getStreamsConfig();
+                // 設定値を取得
+                Properties streamConfig = getStreamsConfig();
 
-        // 設定値とトポロジーを組み合わせてKafka Streamsアプリケーションを定義
-        KafkaStreams streams = new KafkaStreams(topology, streamConfig);
-        streams.start();
+                // 設定値とトポロジーを組み合わせてKafka Streamsアプリケーションを定義
+                KafkaStreams streams = new KafkaStreams(topology, streamConfig);
+                streams.start();
 
-        // グレースフル・シャットダウン
-        Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
-    }
+                // グレースフル・シャットダウン
+                Runtime.getRuntime().addShutdownHook(new Thread(streams::close));
+        }
 
-    private static Properties getStreamsConfig() {
-        Properties streamConfig = new Properties();
+        private static Properties getStreamsConfig() {
+                Properties streamConfig = new Properties();
 
-        // (1) 必須の設定値
-        streamConfig.setProperty("application.id", "filter-streams-app");
-        streamConfig.setProperty("bootstrap.server", BOOTSTRAP_SERVERS);
+                // (1) 必須の設定値
+                streamConfig.setProperty(StreamsConfig.APPLICATION_ID_CONFIG, "filter-streams-app");
+                streamConfig.setProperty(StreamsConfig.BOOTSTRAP_SERVERS_CONFIG, BOOTSTRAP_SERVERS);
 
-        // (2) デフォルトで使用するイベントのキーと値のシリアライズ/デシリアライズ方法を定義
-        streamConfig.setProperty("default.key.serde", Serdes.String().getClass().getName());
-        streamConfig.setProperty("default.value.serde", Serdes.String().getClass().getName());
-        return streamConfig;
-    }
+                // (2) デフォルトで使用するイベントのキーと値のシリアライズ/デシリアライズ方法を定義
+                streamConfig.setProperty(StreamsConfig.DEFAULT_KEY_SERDE_CLASS_CONFIG,
+                                Serdes.String().getClass().getName());
+                streamConfig.setProperty(StreamsConfig.DEFAULT_VALUE_SERDE_CLASS_CONFIG,
+                                Serdes.String().getClass().getName());
 
-    private static Topology buildTopology() {
-        StreamsBuilder streamsBuilder = new StreamsBuilder();
+                return streamConfig;
+        }
 
-        // ① ソースプロセッサー (Topicからイベントを読み取り)
-        KStream<String, TicketOrder> ticketOrderStream = streamsBuilder.stream(SOURCE_TOPIC,
-                Consumed.with(Serdes.String(), new TicketOrderSerdes()));
+        private static Topology buildTopology() {
+                StreamsBuilder streamsBuilder = new StreamsBuilder();
 
-        // ② キーをユーザーIDに付け替え (ReKey)
-        KStream<String, TicketOrder> userKeyOrderStream = ticketOrderStream
-                .map((key, value) -> KeyValue.pair(value.getUserId(), value));
+                // ① ソースプロセッサー (Topicからイベントを読み取り)
+                KStream<String, TicketOrder> ticketOrderStream = streamsBuilder.stream(SOURCE_TOPIC,
+                                Consumed.with(Serdes.String(), new TicketOrderSerdes()));
 
-        // ③ 時間ウィンドウで集約 (5分のウィンドウ、1分の遅延許容)
-        SlidingWindows window = SlidingWindows.ofTimeDifferenceAndGrace(Duration.ofMinutes(5L), Duration.ofMinutes(1L));
-        KTable<Windowed<String>, Integer> orderCountStream = userKeyOrderStream
-                .groupByKey()
-                .windowedBy(window)
-                .aggregate(
-                        () -> 0,
-                        (key, ticketOrder, orderCount) -> orderCount + 1,
-                        Materialized.as("user-order-counts"))
-                .suppress(Suppressed.untilWindowCloses(BufferConfig.unbounded().shutDownWhenFull()));
+                // ② キーをユーザーIDに付け替え (ReKey)
+                KStream<String, TicketOrder> userKeyOrderStream = ticketOrderStream
+                                .map((key, value) -> KeyValue.pair(value.getUserId(), value));
 
-        // ④ 時間あたり取引数の多いユーザーを抽出
-        KStream<String, Integer> suspiciousUserStream = orderCountStream
-                .toStream()
-                .filter((userId, orderCount) -> orderCount >= 3)
-                .map((windowedKey, orderCount) -> KeyValue.pair(windowedKey.key(), orderCount));
+                // ③ 時間ウィンドウで集約 (5分のウィンドウ、1分の遅延許容)
+                SlidingWindows window = SlidingWindows.ofTimeDifferenceAndGrace(Duration.ofMinutes(5L),
+                                Duration.ofMinutes(1L));
+                KTable<Windowed<String>, Integer> orderCountStream = userKeyOrderStream
+                                .groupByKey()
+                                .windowedBy(window)
+                                .aggregate(
+                                                () -> 0,
+                                                (key, ticketOrder, orderCount) -> orderCount + 1,
+                                                Materialized.as("user-order-counts"))
+                                .suppress(Suppressed.untilWindowCloses(BufferConfig.unbounded().shutDownWhenFull()));
 
-        // ⑤ シンクプロセッサー (抽出結果のイベントをTopicに書き戻し)
-        suspiciousUserStream.to(SINK_TOPIC, Produced.with(Serdes.String(), Serdes.Integer()));
+                // ④ 時間あたり取引数の多いユーザーを抽出
+                KStream<String, Integer> suspiciousUserStream = orderCountStream
+                                .toStream()
+                                .filter((userId, orderCount) -> orderCount >= 3)
+                                .map((windowedKey, orderCount) -> KeyValue.pair(windowedKey.key(), orderCount));
 
-        // プロセッサー・トポロジーをビルド
-        return streamsBuilder.build();
-    }
+                // ⑤ シンクプロセッサー (抽出結果のイベントをTopicに書き戻し)
+                suspiciousUserStream.to(SINK_TOPIC, Produced.with(Serdes.String(), Serdes.Integer()));
+
+                // プロセッサー・トポロジーをビルド
+                return streamsBuilder.build();
+        }
 }
