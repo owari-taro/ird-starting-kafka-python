9f8c2a6f350d48208c932b435459deb091476667
Kentaro Sasaki <accelerk@gmail.com>
2022-07-24 18:50:33 +0900

    feat: Added transactional consumer logic

--- transactional-consumer/src/test/java/com/example/ConsumerAppTest.java
+++ transactional-consumer/src/test/java/com/example/ConsumerAppTest.java
@@ -7,32 +7,27 @@ import junit.framework.TestSuite;
 /**
  * Unit test for simple App.
  */
-public class AppTest 
-    extends TestCase
-{
+public class ConsumerAppTest extends TestCase {
     /**
      * Create the test case
      *
      * @param testName name of the test case
      */
-    public AppTest( String testName )
-    {
-        super( testName );
+    public ConsumerAppTest(String testName) {
+        super(testName);
     }
 
     /**
      * @return the suite of tests being tested
      */
-    public static Test suite()
-    {
-        return new TestSuite( AppTest.class );
+    public static Test suite() {
+        return new TestSuite(ConsumerAppTest.class);
     }
 
     /**
      * Rigourous Test :-)
      */
-    public void testApp()
-    {
-        assertTrue( true );
+    public void testApp() {
+        assertTrue(true);
     }
 }
